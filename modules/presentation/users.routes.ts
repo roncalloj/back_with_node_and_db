28486bea745cdb6c8e6b95d0987e15c440f8b16e
roncalloj <r-callo@hotmail.com>
2023-05-06 23:39:44 -0500

    cors enabled for fetch from front

---
+++ modules/presentation/users.routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import UserController from './users.controller';
+
+class Router {
+	router: express.Router;
+	controller: UserController;
+
+	constructor() {
+		this.router = express.Router();
+		this.controller = new UserController();
+		this.mountRoutes();
+	}
+
+	mountRoutes(): void {
+		this.router.get('/', this.controller.getAll);
+		this.router.post('/signup', this.controller.insert);
+	}
+}
+
+export default new Router().router;
---
+++ server_and_db/app.ts
@@ -0,0 +1,32 @@
+import cors from 'cors';
+import express from 'express';
+import RouterUsers from '../modules/presentation/users.routes';
+
+class App {
+	app: express.Application;
+
+	constructor() {
+		this.app = express();
+		this.middlewares();
+		this.mountRoutes();
+	}
+	middlewares(): void {
+		this.app.use(cors());
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+	}
+
+	mountRoutes(): void {
+		this.app.use('/users', RouterUsers);
+
+		this.app.get('/products', (request, response) => {
+			const product = [
+				{ name: 'A', price: 10 },
+				{ name: 'B', price: 20 },
+			];
+			response.json(product);
+		});
+	}
+}
+
+export default new App().app;
